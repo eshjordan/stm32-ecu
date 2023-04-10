4030895b8563fd1c444eda6222a211610b6d8ac5
Jordan Esh <jordan.esh@monashmotorsport.com>
2023-04-10 21:19:03 +1000

    Updated cmake-build.sh script to be more reproducible

--- scripts/cmake-build.sh
+++ scripts/cmake-build.sh
@@ -7,22 +7,27 @@ BUILD_DIR=${SCRIPT_DIR}/../build
 mkdir -p ${BUILD_DIR}
 pushd ${BUILD_DIR}
 
-# Build available targets for x86
-cmake -DBUILD_KERNEL=OFF ..
-cmake --build . -j
-cmake --install .
+SOURCE_STM='unset LD_LIBRARY_PATH && source ~/STM32MP15-Ecosystem-v3.1.0/Developer-Package/SDK/environment-setup-cortexa7t2hf-neon-vfpv4-ostl-linux-gnueabi'
 
-# Cross-compile kernel
-unset LD_LIBRARY_PATH
-source ~/STM32MP15-Ecosystem-v3.1.0/Developer-Package/SDK/environment-setup-cortexa7t2hf-neon-vfpv4-ostl-linux-gnueabi
-cmake -DBUILD_KERNEL=ON ..
-cmake --build . --target kernel_install -j
-cmake --install .
+# TODO: Don't require that code is already generated
+echo "Building available targets for x86. Don't forget to generate code first!"
 
-# Cross-compile all targets
 rm CMakeCache.txt
 cmake -DBUILD_KERNEL=OFF ..
 cmake --build . -j
 cmake --install .
+
+echo "Cross-compile kernel"
+
+rm CMakeCache.txt
+bash -c "${SOURCE_STM} && cmake -DBUILD_KERNEL=ON .."
+bash -c "${SOURCE_STM} && cmake --build . --target kernel_install -j"
+bash -c "${SOURCE_STM} && cmake --install ."
+
+echo "Cross-compile all targets"
+rm CMakeCache.txt
+bash -c "${SOURCE_STM} && cmake -DBUILD_KERNEL=OFF .."
+bash -c "${SOURCE_STM} && cmake --build . -j"
+bash -c "${SOURCE_STM} && cmake --install ."
 
 popd
